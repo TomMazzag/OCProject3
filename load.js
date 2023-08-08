44dfccd781facd60acb5290b3a05e6748c5af99a
Tom Mazzag <itstomazz@gmail.com>
2023-08-08 18:41:38 +0100

    Transition + fixed loader

--- load.js
+++ load.js
@@ -1,7 +1,4 @@
 window.addEventListener("load", () => {
     const loader = document.querySelector(".lds-dual-ring");
     loader.classList.add("lds-dual-ring--hidden");
-    loader.addEventListener("transitionend", () => {
-        document.body.removeChild(document.querySelector(".lds-dual-ring"));
-    })
 })
